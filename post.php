eb00bb6061bd440b1d49756547a6fba7a87f1806
tarunkant <tarunkant05@gmail.com>
2017-08-14 20:25:05 +0530

    Modified

--- post.php
+++ post.php
@@ -1,9 +1,6 @@
 <?php 
-        $String = $_POST['variable'];
-        $String1 = $_POST['variable1'];
-        $ip=file_get_contents('ipinfo.txt');
-        $myfile = fopen("ipinfo.txt", "w") or die("Unable to open file!");
-        fwrite($myfile,$ip.$_POST['variable'].$_POST['variable1']. "\n");
-        fwrite($myfile,"----------------------------------------------". "\n");
-        fclose($myfile);       
+        $String1 = $_POST['intip'];
+        $myfile = fopen("intIP","w") or die("Unable to open file!");
+        fwrite($myfile,$String1."\n");
+        fclose($myfile);               
 ?>

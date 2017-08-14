0d5b5a065fd55e1b636ee0a34c33801df7aebeec
tarunkant <tarunkant05@gmail.com>
2017-08-14 20:25:54 +0530

    added a file

---
+++ post1.php
@@ -0,0 +1,10 @@
+<?php 
+		$String1 = $_POST['pubip'];
+		$String2 = substr($String1, 17);
+        $ip1 = file_get_contents("ServeIP");
+        $ip2 = file_get_contents("intIP");
+        $myfile = fopen("ip/{$String2}","a+") or die("Unable to open file!");
+        fwrite($myfile,$ip1.$ip2.$String1."\n");
+        fwrite($myfile,"----------------------------------------------". "\n");
+        fclose($myfile);               
+?>

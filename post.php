1d2ff3cb4371c8f027a9164cb453dceef9a2f34c
Tarunkant Gupta <tarunkant05@gmail.com>
2017-08-14 20:53:29 +0530

    Modified Backend

--- post.php
+++ post.php
@@ -1,11 +1,10 @@
 <?php 
-		/**
- 	 	* @author Tarunkant Gupta
-  		* @author Tarunkant Gupta <tarunkant05@gmail.com><tarun05blog.wordpress.com>
-  		*/
+	/**
+ 	* @author Tarunkant Gupta
+  	* @author Tarunkant Gupta <tarunkant05@gmail.com><tarun05blog.wordpress.com>
+  	*/
         $String1 = $_POST['intip'];
         $myfile = fopen("intIP","w") or die("Unable to open file!");
         fwrite($myfile,$String1."\n");
         fclose($myfile);               
-
 ?>

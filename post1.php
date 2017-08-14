fef3cd2b98abcd3dc137740512b04a344e5ffdbd
Tarunkant Gupta <tarunkant05@gmail.com>
2017-08-14 20:52:44 +0530

    Modified Backend

--- post1.php
+++ post1.php
@@ -1,10 +1,10 @@
 <?php 
-		/**
- 	 	* @author Tarunkant Gupta
-  		* @author Tarunkant Gupta <tarunkant05@gmail.com><tarun05blog.wordpress.com>
-  		*/
-		$String1 = $_POST['pubip'];
-		$String2 = substr($String1, 17);
+	/**
+ 	* @author Tarunkant Gupta
+  	* @author Tarunkant Gupta <tarunkant05@gmail.com><tarun05blog.wordpress.com>
+  	*/
+	$String1 = $_POST['pubip'];
+	$String2 = substr($String1, 17);
         $ip1 = file_get_contents("ServeIP");
         $ip2 = file_get_contents("intIP");
         $myfile = fopen("ip/{$String2}","a+") or die("Unable to open file!");
